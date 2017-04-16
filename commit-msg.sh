98a597f075fc7a06755540572a776667aea2ec1b
Josh Delman <joshdelman@gmail.com>
2017-04-16 18:17:48 -0400

    Educate my future self

--- commit-msg.sh
+++ commit-msg.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# `commit-msg` hooks do not run in interactive mode.
+# `exec < /dev/tty` returns keyboard input back to stdin.
+# `exec <&-` puts it back.
+
 exec < /dev/tty
 node index.js $1
 exec <&-
